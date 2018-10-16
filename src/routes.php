19954afccf980e9d1053296bf4b00e09ffc4f60f
Josh Anthony <janthony@brokerbin.com>
2018-10-16 13:02:26 -0500

    added showcase route which redirects to display.index

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,9 @@
 <?php
 
 Route::middleware(config('showcase.middleware', ['web', 'auth']))->group(function () {
+    Route::get('showcase', function () {
+        return redirect()->route('displays.index');
+    });
     Route::resource('displays', 'Showcase\App\Http\Controllers\DisplayController');
     Route::resource('trophies', 'Showcase\App\Http\Controllers\TrophyController');
 });
