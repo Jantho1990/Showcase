9808594541992681f96ab130618471edcaad8789
Josh Anthony <janthony@brokerbin.com>
2018-10-16 13:27:30 -0500

    fixed typo in showcase route listing

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,9 @@
 <?php
 
 Route::middleware(config('showcase.middleware', ['web', 'auth']))->group(function () {
-    Route::get('showcase', function () {
+    Route::get('/showcase', function () {
         return redirect()->route('displays.index');
-    });
+    })->name('showcase');
     Route::resource('displays', 'Showcase\App\Http\Controllers\DisplayController');
     Route::resource('trophies', 'Showcase\App\Http\Controllers\TrophyController');
 });
